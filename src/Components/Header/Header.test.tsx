38e9112b910b24a1dcf8d54f09f767b4d42831b5
tmserl <tom.earl@hey.com>
2022-03-22 19:45:28 +0000

    Started testing Header component

---
+++ src/Components/Header/Header.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import Header from './index';
+
+describe('Header', () => {
+  it('renders MakeMeta title and subtitle', () => {
+    render(<Header />);
+
+    const heading = screen.getByText('MakeMeta');
+    const subtitle = screen.getByText('Generate Meta Tags for your website');
+
+    expect(heading).toBeInTheDocument();
+    expect(subtitle).toBeInTheDocument();
+  });
+
+  it('renders Changelog button', () => {
+    render(<Header />);
+
+    const changelog = screen.getByText("What's new?");
+
+    expect(changelog).toBeInTheDocument();
+    expect(changelog).toHaveClass('whats-new');
+  });
+});
